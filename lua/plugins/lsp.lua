90fdb5275f7eb5145e514b51259a0c99b7ccb2cf
a1ts-a1t <unrelatedalt@gmail.com>
2026-07-08 11:17:02 -0700

    fix: treesitter main

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -10,19 +10,11 @@ return {
         branch = "main",
         config = function()
             local ensure_installed = { "lua", "vim", "javascript", "typescript", "python", "markdown" }
-            require("nvim-treesitter.config").setup({
-                ensure_installed = ensure_installed,
-                sync_install = false,
-                auto_install = true,
-                highlight = {
-                    enable = true,
-                    additional_vim_regex_highlighting = false,
-                },
-            })
+            require("nvim-treesitter").install(ensure_installed)
 
-            vim.api.nvim_create_autocmd('FileType', {
+            vim.api.nvim_create_autocmd("FileType", {
                 pattern = ensure_installed,
-                callback = function() vim.treesitter.start() end,
+                callback = function() pcall(vim.treesitter.start) end,
             })
         end
     },
